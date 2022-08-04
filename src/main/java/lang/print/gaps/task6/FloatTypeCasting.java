afea0f196b657463c6e687a65677933500d43a46
Shavkatjon97 <dadshavkatjon97@gmail.com>
2022-08-04 15:06:25 +0500

    Task6 done

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,16 +2,16 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
-        int round;
-        boolean isUp=(numberToBeRounded-0.1)>=0.0;
-        if(isUp){
-            round= (int) Math.ceil(numberToBeRounded);
-        }
-        else {
-            round=(int) Math.floor(numberToBeRounded);
-        }
-        System.out.println(round);
-//        int round = Math.round(numberToBeRounded);
+//        int round;
+//        boolean isUp=(numberToBeRounded-0.1)>=0.0;
+//        if(isUp){
+//            round= (int) Math.ceil(numberToBeRounded);
+//        }
+//        else {
+//            round=(int) Math.floor(numberToBeRounded);
+//        }
 //        System.out.println(round);
+        int round = Math.round(numberToBeRounded);
+        System.out.println(round);
     }
 }
